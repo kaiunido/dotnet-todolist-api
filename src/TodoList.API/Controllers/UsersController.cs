bf2dcb3f70f86263554ec6ca67b318ea64bd1b69
Carlos E. Evangelista <kaiunido@gmail.com>
2026-01-18 14:44:03 -0300

    feat: add [Authorize] attribute to UsersController to enforce authentication

--- src/TodoList.API/Controllers/UsersController.cs
+++ src/TodoList.API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TodoList.API.DTOs;
 using TodoList.API.Services;
@@ -6,6 +7,7 @@ namespace TodoList.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class UsersController(
     IUserService userService
 ) : ControllerBase {
