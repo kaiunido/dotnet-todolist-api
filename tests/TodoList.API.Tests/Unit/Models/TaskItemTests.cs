5a868b44cb0ded1926259fc4dc13267078240ece
Carlos E. Evangelista <kaiunido@gmail.com>
2026-02-08 14:39:41 -0300

    test(models): Add unit tests for TaskItem model
    - Added tests to cover behavior of TaskItem methods:
      - Constructor validation and normalization of description.
      - Description update with valid and invalid values.
      - Completion status changes with MarkAsDone and UnmarkAsDone methods.

---
+++ tests/TodoList.API.Tests/Unit/Models/TaskItemTests.cs
@@ -0,0 +1,76 @@
+using TodoList.API.Models;
+
+namespace TodoList.API.Tests.Unit.Models;
+
+public class TaskItemTests
+{
+    [Fact]
+    public void Ctor_ShouldNormalizeDescription()
+    {
+        var task = new TaskItem(1, "  test description  ");
+
+        Assert.Equal("test description", task.Description);
+        Assert.False(task.IsDone);
+        Assert.Null(task.DoneAt);
+    }
+
+    [Fact]
+    public void Ctor_WithEmptyDescription_ShouldThrowArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new TaskItem(1, "   "));
+    }
+
+    [Fact]
+    public void UpdateDescription_WithSanemValueAfterTrim_ShouldNotChange()
+    {
+        var task = new TaskItem(1, "test description");
+        var oldUpdatedAt = task.UpdatedAt;
+
+        task.UpdateDescription(" test description  ");
+
+        Assert.Equal("test description", task.Description);
+        Assert.Equal(oldUpdatedAt, task.UpdatedAt);
+    }
+
+    [Fact]
+    public void UpdateDescription_WithEmpty_ShouldThrowArgumentException()
+    {
+        var task = new TaskItem(1, "test description");
+
+        Assert.Throws<ArgumentException>(() => task.UpdateDescription("   "));
+    }
+
+    [Fact]
+    public void MarkAsDone_ShouldSetDoneAt()
+    {
+        var task = new TaskItem(1, "test description");
+
+        task.MarkAsDone();
+
+        Assert.True(task.IsDone);
+        Assert.NotNull(task.DoneAt);
+    }
+
+    [Fact]
+    public void MarkAsDone_WhenAlreadyDone_ShouldNotChangeDoneAt()
+    {
+        var task = new TaskItem(1, "test description", true);
+        var firstDoneAt = task.DoneAt;
+
+        task.MarkAsDone();
+
+        Assert.True(task.IsDone);
+        Assert.Equal(firstDoneAt, task.DoneAt);
+    }
+
+    [Fact]
+    public void UnmarkAsDone_ShouldClearDoneAt()
+    {
+        var task = new TaskItem(1, "test descriptions", true);
+
+        task.UnmarkAsDone();
+
+        Assert.False(task.IsDone);
+        Assert.Null(task.DoneAt);
+    }
+}
